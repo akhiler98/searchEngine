be554a1068ee1964dcf6fe823c66f872a417e84a
akhiler <akhiler19@gmail.com>
2024-05-19 13:45:17 +0200

    api key stored in .env file

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,13 +9,14 @@ class SearchController extends Controller
 {
      public function index($searchText)
     {
-
+        $apiKey = env('GOOGLE_API_KEY');
+        $cx = env('GOOGLE_CX');
         $text = $searchText;
         $client = new Client();
         $response = $client->request('GET', 'https://www.googleapis.com/customsearch/v1', [
             'query' => [
-                'key' => 'AIzaSyBVH31n7_s9RWt7gbb15lHpqG07y-wk-6I',
-                'cx' => 'd09cf1c841863460c',
+                'key' =>  $apiKey,
+                'cx' => $cx,
                 'q' => $text
             ]
         ]);
